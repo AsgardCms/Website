964b52c5feac11a8c9c230233f408685b657c1ad
Nicolas Widart <n.widart@gmail.com>
2014-11-11 14:19:27 +0100

    Adding box slider

--- resources/views/public/partials/header.blade.php
+++ resources/views/public/partials/header.blade.php
@@ -7,12 +7,14 @@
 		<meta name="keywords" content="" />
 		<!--[if lte IE 8]><script src="assets/css/ie/html5shiv.js"></script><![endif]-->
 		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/jquery.bxslider.min.js"></script>
 		<script src="assets/js/jquery.dropotron.min.js"></script>
 		<script src="assets/js/jquery.scrollgress.min.js"></script>
 		<script src="assets/js/skel.min.js"></script>
 		<script src="assets/js/skel-layers.min.js"></script>
 		<script src="assets/js/init.js"></script>
 		<link rel="stylesheet" href="assets/css/style.css" />
+		<link rel="stylesheet" href="assets/css/jquery.bxslider.css" />
 		<noscript>
 			<link rel="stylesheet" href="assets/css/skel.css" />
 			<link rel="stylesheet" href="assets/css/style.css" />
