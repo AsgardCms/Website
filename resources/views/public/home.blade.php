d882b2952721539b3f95e992ce80a8931d4f92a0
Nicolas Widart <n.widart@gmail.com>
2014-11-11 13:32:08 +0100

    Removing unused links

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -23,7 +23,7 @@
                 Asguard CMS will kickstart all your future client applications.
             </p>
         </header>
-        <span class="image featured"><img src="images/pic01.jpg" alt="" /></span>
+        <span class="image featured"><img src="assets/images/pic01.jpg" alt="" /></span>
     </section>
 
     <section class="box special features">
@@ -56,7 +56,7 @@
     <div class="row">
         <div class="6u 12u(2)">
             <section class="box special">
-                <span class="image featured"><img src="images/pic02.jpg" alt="" /></span>
+                <span class="image featured"><img src="assets/images/pic02.jpg" alt="" /></span>
                 <h3>Sed lorem adipiscing</h3>
                 <p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
                 <ul class="actions">
@@ -67,7 +67,7 @@
         <div class="6u 12u(2)">
 
             <section class="box special">
-                <span class="image featured"><img src="images/pic03.jpg" alt="" /></span>
+                <span class="image featured"><img src="assets/images/pic03.jpg" alt="" /></span>
                 <h3>Accumsan integer</h3>
                 <p>Integer volutpat ante et accumsan commophasellus sed aliquam feugiat lorem aliquet ut enim rutrum phasellus iaculis accumsan dolore magna aliquam veroeros.</p>
                 <ul class="actions">
