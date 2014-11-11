5b442ddad3c6a2c3e74dfc0d0be4089a91b5a9ab
Nicolas Widart <n.widart@gmail.com>
2014-11-11 19:30:20 +0100

    Adding active styles

--- resources/views/public/partials/navigation.blade.php
+++ resources/views/public/partials/navigation.blade.php
@@ -1,7 +1,7 @@
 <h1><a href="{{ URL::route('home') }}">Asguard</a> CMS</h1>
 <nav id="nav">
     <ul>
-        <li><a href="{{ URL::route('home') }}">Home</a></li>
-        <li><a href="{{ URL::route('install') }}" class="button">Install</a></li>
+        <li><a href="{{ URL::route('home') }}" class="{{ Request::is('/') ? 'active' : ''}}">Home</a></li>
+        <li><a href="{{ URL::route('install') }}" class="button {{ Request::is('install') ? 'active' : ''}}">Install</a></li>
     </ul>
 </nav>
