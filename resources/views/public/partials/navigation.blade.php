794b249c28dc0770ecf9e80837daf8125962dbda
Nicolas Widart <n.widart@gmail.com>
2014-11-11 13:52:02 +0100

    Adding homelink

--- resources/views/public/partials/navigation.blade.php
+++ resources/views/public/partials/navigation.blade.php
@@ -1,7 +1,7 @@
 <h1><a href="{{ URL::route('home') }}">Asguard</a> CMS</h1>
 <nav id="nav">
     <ul>
-        <li><a href="index.html">Home</a></li>
+        <li><a href="{{ URL::route('home') }}">Home</a></li>
         <li><a href="#" class="button">Install</a></li>
     </ul>
 </nav>
