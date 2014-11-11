05e8e11af6d7502ebb9e2d2d16cf39c53978a198
Nicolas Widart <n.widart@gmail.com>
2014-11-11 14:50:02 +0100

    Add installation instructions

--- resources/views/public/install.blade.php
+++ resources/views/public/install.blade.php
@@ -4,23 +4,53 @@
 <!-- Main -->
 <section id="main" class="container">
     <header>
-        <h2>Generic</h2>
-        <p>A generic page for every non-generic situation.</p>
+        <h2>Installation</h2>
+        <p>Install Asguard CMS in less then 5 minutes!</p>
     </header>
     <div class="box">
-        <span class="image featured"><img src="images/pic01.jpg" alt="" /></span>
-        <h3>This is a subheading</h3>
-        <p>Cep risus aliquam gravida cep ut lacus amet. Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae ante vestibulum ante. Blandit. Ante accumsan nisi eu placerat gravida placerat adipiscing in risus fusce vitae ac mi accumsan nunc in accumsan tempor blandit aliquet aliquet lobortis. Ultricies blandit lobortis praesent turpis. Adipiscing accumsan adipiscing adipiscing ac lacinia cep. Orci blandit a iaculis adipiscing ac. Vivamus ornare laoreet odio vis praesent nunc lorem mi. Erat. Tempus sem faucibus ac id. Vis in blandit. Nascetur ultricies blandit ac. Arcu aliquam. Accumsan mi eget adipiscing nulla. Non vestibulum ac interdum condimentum semper commodo massa arcu.</p>
-        <div class="row">
-            <div class="6u 12u(3)">
-                <h3>And now a subheading</h3>
-                <p>Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae lorem ipsum adipiscing ac. Vivamus ornare laoreet odio vis praesent.</p>
-            </div>
-            <div class="6u 12u(3)">
-                <h3>And another subheading</h3>
-                <p>Adipiscing faucibus nunc placerat. Tempus adipiscing turpis non blandit accumsan eget lacinia nunc integer interdum amet aliquam ut orci non col ut ut praesent. Semper amet interdum mi. Phasellus enim laoreet ac ac commodo faucibus faucibus. Curae lorem ipsum adipiscing ac. Vivamus ornare laoreet odio vis praesent.</p>
-            </div>
-        </div>
+        <h3>Get the code</h3>
+        <p>
+            <ol>
+                <li>
+                    <p>
+                        First you can get the code using the following command: <br/>
+                        <code>
+                            composer create-project nwidart/platform your-project-name --prefer-dist --stability=dev
+                        </code>
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        Next, install the composer dependencies: <br/>
+                        <code>
+                            composer install
+                        </code>
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        Finally, run the install command to get you started:
+                        <code style="display: block;">
+                            php artisan platform:install
+                        </code>
+                        This will do the following:
+                        <ul>
+                            <li>Setup database information</li>
+                            <li>Running migrations</li>
+                            <li>Running seeds</li>
+                            <li>Publishing assets</li>
+                            <li>Create a first admin account</li>
+                        </ul>
+                    </p>
+                </li>
+                <li>
+                    <p>
+                        Done! <br/>
+                        Enjoy your freshly installed website.
+                    </p>
+                </li>
+            </ol>
+        </p>
     </div>
 </section>
 
