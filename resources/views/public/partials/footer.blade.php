c0c2ce2e475b8b80f83cae3fdde478e022012c67
Nicolas Widart <n.widart@gmail.com>
2014-11-11 13:33:02 +0100

    Removing footer links

--- resources/views/public/partials/footer.blade.php
+++ resources/views/public/partials/footer.blade.php
@@ -1,11 +1,7 @@
 <footer id="footer">
     <ul class="icons">
-        <li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-        <li><a href="#" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
-        <li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
-        <li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li>
-        <li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
-        <li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+        <li><a href="#" class="icon fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
+        <li><a href="#" class="icon fa-github" target="_blank"><span class="label">Github</span></a></li>
     </ul>
     <ul class="copyright">
         <li>{{ date('Y') }} &copy; Asguard. All rights reserved.</li>
