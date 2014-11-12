78acefe75066ef8742a4f7963c06379526e0421c
Nicolas Widart <n.widart@gmail.com>
2014-11-12 12:44:30 +0100

    Adding entries migration file

---
+++ database/migrations/2014_11_12_114239_create_entries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('entries');
+    }
+
+}
