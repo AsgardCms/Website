1bb68683ee4bf9c3bddfd69f31827cbbc6e30d3c
Nicolas Widart <n.widart@gmail.com>
2014-11-12 12:41:35 +0100

    Fixing namespace

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,7 +5,7 @@ use Illuminate\Routing\Router;
 $router->get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
 $router->get('install', ['as' => 'install', 'uses' => 'FrontController@getInstallPage']);
 
-$router->group(['prefix' => 'api', 'namespace' => 'Asguard\Controllers\Api'], function(Router $router)
+$router->group(['prefix' => 'api', 'namespace' => 'Api'], function(Router $router)
 {
     $router->post('subscribe', ['as' => 'subscribe', 'uses' => 'EmailController@subscribe']);
 });
