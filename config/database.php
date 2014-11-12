c31215576d12e9a0808ffaaaf25a4ae65899b6a7
Nicolas Widart <n.widart@gmail.com>
2014-11-12 22:05:24 +0100

    Configure database

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
+			'database'  => 'asgard',
 			'username'  => getenv('DB_USERNAME'),
 			'password'  => getenv('DB_PASSWORD'),
 			'charset'   => 'utf8',
