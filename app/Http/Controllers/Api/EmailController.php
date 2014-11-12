e96185a90acd86678eaf13f954af75e11e028d27
Nicolas Widart <n.widart@gmail.com>
2014-11-12 13:01:08 +0100

    Inject the entry repository

--- app/Http/Controllers/Api/EmailController.php
+++ app/Http/Controllers/Api/EmailController.php
@@ -1,9 +1,22 @@
 <?php namespace Asguard\Http\Controllers\Api;
 
+use Asguard\Entries\Repositories\EntryRepository;
 use Asguard\Http\Controllers\Controller;
 
 class EmailController extends Controller
 {
+    /**
+     * @var EntryRepository
+     */
+    private $entry;
+
+    public function __construct(EntryRepository $entry)
+    {
+        parent::__construct();
+
+        $this->entry = $entry;
+    }
+
     public function subscribe()
     {
         dd('called');
