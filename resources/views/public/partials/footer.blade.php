226d384c413e900b7a510faf16ca567d64e7a379
Nicolas Widart <n.widart@gmail.com>
2014-11-13 20:13:39 +0100

    Using the dedicated twitter account

--- resources/views/public/partials/footer.blade.php
+++ resources/views/public/partials/footer.blade.php
@@ -1,6 +1,6 @@
         <footer id="footer">
             <ul class="icons">
-                <li><a href="https://twitter.com/nicolaswidart" class="icon fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
+                <li><a href="https://twitter.com/AsgardCMS" class="icon fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
                 <li><a href="https://github.com/AsgardCms" class="icon fa-github" target="_blank"><span class="label">Github</span></a></li>
             </ul>
             <ul class="copyright">
