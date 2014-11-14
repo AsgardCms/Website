247eb38000aa22a5db3fa5c483f8d6990e74dd91
Nicolas Widart <n.widart@gmail.com>
2014-11-14 09:05:57 +0100

    SEO: Adding meta data.

--- resources/views/public/partials/header.blade.php
+++ resources/views/public/partials/header.blade.php
@@ -3,8 +3,15 @@
 	<head>
 		<title>Asgard CMS</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
+		<meta name="description" content="A modular multilingual Content Management System built with Laravel 5" />
+		<meta name="keywords" content="laravel, CMS, Modular, Multilingual" />
+
+		<meta property="og:title" content="A modular multilingual Content Management System built with Laravel 5" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://asgardcms.com" />
+        <meta property="og:description" content="Introducing a full-featured modular multilingual CMS built on top of the Laravel framework. Asgard CMS will kickstart all your future client applications." />
+        <meta property="og:image" content="assets/images/thor-the-dark-world-asgard.jpg" />
+
 		<!--[if lte IE 8]><script src="assets/css/ie/html5shiv.js"></script><![endif]-->
 		<script src="assets/js/jquery.min.js"></script>
 		<script src="assets/js/jquery.bxslider.min.js"></script>
