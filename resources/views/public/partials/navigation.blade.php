d3a73c0618f0c803f6a71a7402a91a2d1fef9702
Nicolas Widart <n.widart@gmail.com>
2014-11-16 18:35:52 +0100

    Add the FAQ link

--- resources/views/public/partials/navigation.blade.php
+++ resources/views/public/partials/navigation.blade.php
@@ -3,5 +3,6 @@
     <ul>
         <li><a href="{{ URL::route('home') }}" class="{{ Request::is('/') ? 'active' : ''}}">Home</a></li>
         {{--<li><a href="{{ URL::route('install') }}" class="button {{ Request::is('install') ? 'active' : ''}}">Install</a></li>--}}
+        <li><a href="{{ URL::route('faq') }}" class="{{ Request::is('faq') ? 'active' : ''}}">F.A.Q.</a></li>
     </ul>
 </nav>
