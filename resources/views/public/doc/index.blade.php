5651d73eafa512841266f59e4a5e0d3b78193768
Nicolas Widart <n.widart@gmail.com>
2014-12-14 20:43:41 +0100

    Add page header

--- resources/views/public/doc/index.blade.php
+++ resources/views/public/doc/index.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 <section id="main" class="container">
+    <header>
+        <h2>Documentation</h2>
+        <p>All your questions answered</p>
+    </header>
     <div class="row box row-box">
         <div class="3u">
             @include('public.doc.partials.sidebar')
