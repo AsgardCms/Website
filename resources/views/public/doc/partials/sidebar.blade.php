ce1e95e10df82f0ce1351dcd8806248081c7b942
Nicolas Widart <n.widart@gmail.com>
2014-12-14 20:58:02 +0100

    Adding the sidebar linsk

--- resources/views/public/doc/partials/sidebar.blade.php
+++ resources/views/public/doc/partials/sidebar.blade.php
@@ -6,62 +6,56 @@
     <li class="{{ Request::is('*/getting-started/installation') ? 'active' : ''}}">
         <a href="{{ route('doc.show', ['getting-started/installation']) }}">Installation</a>
     </li>
-    <li>
-        <a href="#test">Install</a>
-    </li>
     <li class="header">
         <i class="fa fa-bolt"></i>
         Core Module
     </li>
-    <li class="">
-        <a href="#test">Configuration</a>
+    <li class="{{ Request::is('*/core-module/configuration') ? 'active' : ''}}">
+        <a href="{{ route('doc.show', ['core-module/configuration']) }}">Configuration</a>
     </li>
-    <li>
-        <a href="#test">Navigation</a>
+    <li class="{{ Request::is('*/core-module/navigation') ? 'active' : ''}}">
+        <a href="{{ route('doc.show', ['core-module/navigation']) }}">Navigation</a>
     </li>
-    <li>
-        <a href="#test">Permissions</a>
+    <li class="{{ Request::is('*/core-module/permissions') ? 'active' : ''}}">
+        <a href="{{ route('doc.show', ['core-module/permissions']) }}">Permissions</a>
     </li>
-    <li>
-        <a href="#test">Repositories</a>
+    <li class="{{ Request::is('*/core-module/repositories') ? 'active' : ''}}">
+        <a href="{{ route('doc.show', ['core-module/repositories']) }}">Repositories</a>
     </li>
     <li class="header">
         <i class="fa fa-file-image-o"></i>
         Media Module
     </li>
-    <li>
-        <a href="#test">Thumbnails</a>
+    <li class="{{ Request::is('*/media-module/thumbnails') ? 'active' : ''}}">
+        <a href="{{ route('doc.show', ['media-module/thumbnails']) }}">Thumbnails</a>
     </li>
-    <li>
-        <a href="#test">Getting a thumbnail</a>
+    <li class="{{ Request::is('*/media-module/getting-a-thumbnail') ? 'active' : ''}}">
+        <a href="{{ route('doc.show', ['media-module/getting-a-thumbnail']) }}">Getting a thumbnail</a>
     </li>
-    <li>
-        <a href="#test">Refreshing thumbnails</a>
+    <li class="{{ Request::is('*/media-module/refreshing-thumbnails') ? 'active' : ''}}">
+        <a href="{{ route('doc.show', ['media-module/refreshing-thumbnails']) }}">Refreshing thumbnails</a>
     </li>
     <li class="header">
         <i class="fa fa-cogs"></i>
         Setting Module
     </li>
-    <li>
-        <a href="#test">Adding settings</a>
+    <li class="{{ Request::is('*/setting-module/adding-settings') ? 'active' : ''}}">
+        <a href="{{ route('doc.show', ['setting-module/adding-settings']) }}">Adding settings</a>
     </li>
-    <li>
-        <a href="#test">Available fields</a>
+    <li class="{{ Request::is('*/setting-module/available-fields') ? 'active' : ''}}">
+        <a href="{{ route('doc.show', ['setting-module/available-fields']) }}">Available fields</a>
     </li>
-    <li>
-        <a href="#test">Custom fields</a>
+    <li class="{{ Request::is('*/setting-module/custom-fields') ? 'active' : ''}}">
+        <a href="{{ route('doc.show', ['setting-module/custom-fields']) }}">Custom fields</a>
     </li>
-    <li>
-        <a href="#test">Reading settings</a>
-    </li>
-    <li>
-        <a href="#test">Repositories</a>
+    <li class="{{ Request::is('*/setting-module/reading-settings') ? 'active' : ''}}">
+        <a href="{{ route('doc.show', ['setting-module/reading-settings']) }}">Reading settings</a>
     </li>
     <li class="header">
         <i class="fa fa-picture-o"></i>
         Themes
     </li>
-    <li>
-        <a href="#test">Index</a>
+    <li class="{{ Request::is('*/themes/usage') ? 'active' : ''}}">
+        <a href="{{ route('doc.show', ['themes/usage']) }}">Usage</a>
     </li>
 </ul>
