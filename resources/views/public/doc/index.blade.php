bfe636fc0c72a4df9b4a505a73c626648709896a
Nicolas Widart <n.widart@gmail.com>
2014-12-14 14:10:22 +0100

    Adding more navigation links

--- resources/views/public/doc/index.blade.php
+++ resources/views/public/doc/index.blade.php
@@ -6,8 +6,8 @@
         <div class="3u">
             <ul class="side-menu">
                 <li class="header">
-                    <i class="fa fa-bolt"></i>
-                    Core
+                    <i class="fa fa-home"></i>
+                    Getting Started
                 </li>
                 <li class="{{ Request::is('*/getting-started/installation') ? 'active' : ''}}">
                     <a href="{{ route('doc.show', ['getting-started/installation']) }}">Installation</a>
@@ -17,13 +17,58 @@
                 </li>
                 <li class="header">
                     <i class="fa fa-bolt"></i>
-                    Core
+                    Core Module
                 </li>
-                <li class="active">
-                    <a href="#test">Install</a>
+                <li class="">
+                    <a href="#test">Configuration</a>
                 </li>
                 <li>
-                    <a href="#test">Install</a>
+                    <a href="#test">Navigation</a>
+                </li>
+                <li>
+                    <a href="#test">Permissions</a>
+                </li>
+                <li>
+                    <a href="#test">Repositories</a>
+                </li>
+                <li class="header">
+                    <i class="fa fa-file-image-o"></i>
+                    Media Module
+                </li>
+                <li>
+                    <a href="#test">Thumbnails</a>
+                </li>
+                <li>
+                    <a href="#test">Getting a thumbnail</a>
+                </li>
+                <li>
+                    <a href="#test">Refreshing thumbnails</a>
+                </li>
+                <li class="header">
+                    <i class="fa fa-cogs"></i>
+                    Setting Module
+                </li>
+                <li>
+                    <a href="#test">Adding settings</a>
+                </li>
+                <li>
+                    <a href="#test">Available fields</a>
+                </li>
+                <li>
+                    <a href="#test">Custom fields</a>
+                </li>
+                <li>
+                    <a href="#test">Reading settings</a>
+                </li>
+                <li>
+                    <a href="#test">Repositories</a>
+                </li>
+                <li class="header">
+                    <i class="fa fa-picture-o"></i>
+                    Themes
+                </li>
+                <li>
+                    <a href="#test">Index</a>
                 </li>
             </ul>
         </div>
