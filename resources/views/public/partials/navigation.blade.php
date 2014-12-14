10bd88ba95fe324cd23ac84c4342600ba6dbae4f
Nicolas Widart <n.widart@gmail.com>
2014-12-14 20:10:17 +0100

    Add documentation link in navigation

--- resources/views/public/partials/navigation.blade.php
+++ resources/views/public/partials/navigation.blade.php
@@ -4,5 +4,6 @@
         <li><a href="{{ URL::route('home') }}" class="{{ Request::is('/') ? 'active' : ''}}">Home</a></li>
         {{--<li><a href="{{ URL::route('install') }}" class="button {{ Request::is('install') ? 'active' : ''}}">Install</a></li>--}}
         <li><a href="{{ URL::route('faq') }}" class="{{ Request::is('faq') ? 'active' : ''}}">F.A.Q.</a></li>
+        <li><a href="{{ URL::route('doc.index') }}" class="{{ Request::is('docs*') ? 'active' : ''}}">Documentation</a></li>
     </ul>
 </nav>
