cc3c334a589b884467f52b61cc8983b628f4dd74
Nicolas Widart <n.widart@gmail.com>
2014-12-14 12:54:48 +0100

    Set relative to root

--- public/assets/js/init.js
+++ public/assets/js/init.js
@@ -9,13 +9,13 @@
 	skel.init({
 		reset: 'full',
 		breakpoints: {
-			global:		{ range: '*', href: 'assets/css/style.css', containers: '60em', grid: { gutters: ['2em', 0] } },
-			wide:		{ range: '-1680', href: 'assets/css/style-wide.css' },
-			normal:		{ range: '-1280', href: 'assets/css/style-normal.css', viewport: { scalable: false } },
-			narrow:		{ range: '-980', href: 'assets/css/style-narrow.css', containers: '90%' },
-			narrower:	{ range: '-840', href: 'assets/css/style-narrower.css', containers: '90%!', grid: { zoom: 2 } },
-			mobile:		{ range: '-736', href: 'assets/css/style-mobile.css', containers: '100%!' },
-			mobilep:	{ range: '-480', href: 'assets/css/style-mobilep.css', grid: { zoom: 3 } }
+			global:		{ range: '*', href: '/assets/css/style.css', containers: '60em', grid: { gutters: ['2em', 0] } },
+			wide:		{ range: '-1680', href: '/assets/css/style-wide.css' },
+			normal:		{ range: '-1280', href: '/assets/css/style-normal.css', viewport: { scalable: false } },
+			narrow:		{ range: '-980', href: '/assets/css/style-narrow.css', containers: '90%' },
+			narrower:	{ range: '-840', href: '/assets/css/style-narrower.css', containers: '90%!', grid: { zoom: 2 } },
+			mobile:		{ range: '-736', href: '/assets/css/style-mobile.css', containers: '100%!' },
+			mobilep:	{ range: '-480', href: '/assets/css/style-mobilep.css', grid: { zoom: 3 } }
 		},
 		plugins: {
 			layers: {
