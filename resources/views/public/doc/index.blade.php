e1519799f4ecc45c6c41dd7ae5eb297680a2f135
Nicolas Widart <n.widart@gmail.com>
2014-12-14 11:10:43 +0100

    Add a docs stylesheet

---
+++ resources/views/public/doc/index.blade.php
@@ -0,0 +1,25 @@
+@extends('public.layouts.master')
+
+@section('content')
+<section id="main" class="container">
+    <div class="row box row-box">
+        <div class="2u">
+            <ul class="side-menu">
+                <li class="header">
+                    <i class="fa fa-bolt"></i>
+                    Core
+                </li>
+                <li>
+                    <a href="#test">Install</a>
+                </li>
+                <li>
+                    <a href="#test">Install</a>
+                </li>
+            </ul>
+        </div>
+        <div class="10u">
+            <h3>Doc</h3>
+        </div>
+    </div>
+</section>
+@stop
