e7eb94b60fcf695e798da384e9181afaf82577d5
Nicolas Widart <n.widart@gmail.com>
2014-12-14 13:59:56 +0100

    Name the doc.show route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,10 +6,9 @@ $router->get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
 $router->get('install', ['as' => 'install', 'uses' => 'FrontController@getInstallPage']);
 $router->get('faq', ['as' => 'faq', 'uses' => 'FrontController@getFaq']);
 
-$router->group(['prefix' => 'api', 'namespace' => 'Api'], function(Router $router)
-{
+$router->group(['prefix' => 'api', 'namespace' => 'Api'], function (Router $router) {
     $router->post('subscribe', ['as' => 'subscribe', 'uses' => 'EmailController@subscribe']);
 });
 
 $router->get('docs', ['as' => 'doc.index', 'uses' => 'DocumentationController@index']);
-$router->get('docs/{page}', 'DocumentationController@show')->where('page', '.*');
+$router->get('docs/{page}', ['as' => 'doc.show', 'uses' => 'DocumentationController@show'])->where('page', '.*');
