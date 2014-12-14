dff55cac6a7d833865e6815cb5352f5dd2d287c6
Nicolas Widart <n.widart@gmail.com>
2014-12-14 12:45:40 +0100

    Use the asset helper

--- resources/views/public/partials/header.blade.php
+++ resources/views/public/partials/header.blade.php
@@ -10,24 +10,24 @@
         <meta property="og:type" content="website" />
         <meta property="og:url" content="http://asgardcms.com" />
         <meta property="og:description" content="Introducing a full-featured modular multilingual CMS built on top of the Laravel framework. Asgard CMS will kickstart all your future client applications." />
-        <meta property="og:image" content="{{ asset('assets/images/thor-the-dark-world-asgard.jpg')}}" />
+        <meta property="og:image" content="{{ asset('/assets/images/thor-the-dark-world-asgard.jpg')}}" />
 
 		<!--[if lte IE 8]><script src="assets/css/ie/html5shiv.js"></script><![endif]-->
-		<script src="assets/js/jquery.min.js"></script>
-		<script src="assets/js/jquery.bxslider.min.js"></script>
-		<script src="assets/js/jquery.scrollTo.min.js"></script>
-		<script src="assets/js/jquery.localScroll.min.js"></script>
-		<script src="assets/js/jquery.dropotron.min.js"></script>
-		<script src="assets/js/jquery.scrollgress.min.js"></script>
-		<script src="assets/js/skel.min.js"></script>
-		<script src="assets/js/skel-layers.min.js"></script>
-		<script src="assets/js/init.js"></script>
-		<link rel="stylesheet" href="assets/css/jquery.bxslider.css" />
+		<script src="{{ asset('/assets/js/jquery.min.js') }}"></script>
+		<script src="{{ asset('/assets/js/jquery.bxslider.min.js') }}"></script>
+		<script src="{{ asset('/assets/js/jquery.scrollTo.min.js') }}"></script>
+		<script src="{{ asset('/assets/js/jquery.localScroll.min.js') }}"></script>
+		<script src="{{ asset('/assets/js/jquery.dropotron.min.js') }}"></script>
+		<script src="{{ asset('/assets/js/jquery.scrollgress.min.js') }}"></script>
+		<script src="{{ asset('/assets/js/skel.min.js') }}"></script>
+		<script src="{{ asset('/assets/js/skel-layers.min.js') }}"></script>
+		<script src="{{ asset('/assets/js/init.js') }}"></script>
+		<link rel="stylesheet" href="{{ asset('/assets/css/jquery.bxslider.css') }}" />
 		<noscript>
-			<link rel="stylesheet" href="assets/css/skel.css" />
-			<link rel="stylesheet" href="assets/css/style.css" />
-			<link rel="stylesheet" href="assets/css/style-wide.css" />
+			<link rel="stylesheet" href="{{ asset('/assets/css/skel.css') }}" />
+			<link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}" />
+			<link rel="stylesheet" href="{{ asset('/assets/css/style-wide.css') }}" />
 		</noscript>
-		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="{{ asset('/assets/css/ie/v8.css')}}" /><![endif]-->
 	</head>
 	@include("public.partials.{$header}-header")
