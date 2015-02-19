ef25477783995868edcb2d87b409020b6f656589
Nicolas Widart <n.widart@gmail.com>
2015-02-19 20:25:11 +0100

    Add a validate github request

---
+++ app/Http/Requests/GithubUsernameRequest.php
@@ -0,0 +1,18 @@
+<?php namespace Asgard\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GithubUsernameRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'username' => 'required',
+        ];
+    }
+
+    public function authorize()
+    {
+        return true;
+    }
+}
