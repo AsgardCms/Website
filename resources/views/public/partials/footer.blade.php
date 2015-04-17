8d9e99f7b01c5dbaeebc84afabc307643982a3b7
Nicolas Widart <n.widart@gmail.com>
2015-04-17 20:13:57 +0200

    Adding slack link on footer

--- resources/views/public/partials/footer.blade.php
+++ resources/views/public/partials/footer.blade.php
@@ -2,6 +2,9 @@
             <ul class="icons">
                 <li><a href="https://twitter.com/AsgardCMS" class="icon fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
                 <li><a href="https://github.com/AsgardCms" class="icon fa-github" target="_blank"><span class="label">Github</span></a></li>
+                <li>
+                    <script async defer src="http://slack.asgardcms.com/slackin.js"></script>
+                </li>
             </ul>
             <ul class="copyright">
                 <li>{{ date('Y') }} &copy; Asgard. All rights reserved.</li>
