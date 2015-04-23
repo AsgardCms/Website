20b0cc435b20549a8e9954e17aa79ebda93aeab1
Nicolas Widart <n.widart@gmail.com>
2015-04-23 20:35:00 +0200

    Adding link to documentation

--- Modules/Documentation/Composers/SidebarViewComposer.php
+++ Modules/Documentation/Composers/SidebarViewComposer.php
@@ -1,11 +1,21 @@
 <?php namespace Modules\Documentation\Composers;
 
 use Illuminate\Contracts\View\View;
+use Maatwebsite\Sidebar\SidebarGroup;
+use Maatwebsite\Sidebar\SidebarItem;
 use Modules\Core\Composers\BaseSidebarViewComposer;
 
 class SidebarViewComposer extends BaseSidebarViewComposer
 {
     public function compose(View $view)
     {
+        $view->sidebar->group(trans('core::sidebar.content'), function (SidebarGroup $group) {
+            $group->addItem('Documentation', function (SidebarItem $item) {
+                $item->icon = 'fa fa-files-o';
+                $item->weight = 0;
+                $item->route('admin.docs.index');
+                $item->authorize(true);
+            });
+        });
     }
 }
