2680b207da4d3a6e453f960c42fc96b28085ef59
Nicolas Widart <n.widart@gmail.com>
2015-04-23 20:34:47 +0200

    Adding doc index

--- Modules/Documentation/Http/backendRoutes.php
+++ Modules/Documentation/Http/backendRoutes.php
@@ -4,6 +4,9 @@ use Illuminate\Routing\Router;
 
 /** @var Router $router */
 
-$router->group(['prefix' =>'/documentation'], function (Router $router) {
-// append
+$router->group(['prefix' => '/documentation'], function (Router $router) {
+    $router->get('docs', [
+        'as' => 'admin.docs.index',
+        'uses' => 'DocumentationController@index',
+    ]);
 });
