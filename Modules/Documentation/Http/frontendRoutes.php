a1fae125a3d92683023448030df81cfaf2db1ce1
Nicolas Widart <n.widart@gmail.com>
2015-04-23 20:42:28 +0200

    Add the doc front end routes

--- Modules/Documentation/Http/frontendRoutes.php
+++ Modules/Documentation/Http/frontendRoutes.php
@@ -1,1 +1,7 @@
 <?php
+use Illuminate\Routing\Router;
+
+/** @var Router $router */
+
+$router->get('docs', ['as' => 'doc.index', 'uses' => 'Frontend\DocumentationController@index']);
+$router->get('docs/{page}', ['as' => 'doc.show', 'uses' => 'Frontend\DocumentationController@show'])->where('page', '.*');
