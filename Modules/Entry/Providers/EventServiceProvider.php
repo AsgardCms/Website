ac83a911e1406dc84d3a20de45a5670830e3a835
Nicolas Widart <n.widart@gmail.com>
2015-04-24 21:12:29 +0200

    Add and load an event service provider

---
+++ Modules/Entry/Providers/EventServiceProvider.php
@@ -0,0 +1,12 @@
+<?php namespace Modules\Entry\Providers;
+
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    protected $listen = [
+        'Modules\Entry\Events\EntryWasInvited' => [
+            'Modules\Entry\Events\Handlers\SendInviteFinalStepEmail',
+        ],
+    ];
+}
