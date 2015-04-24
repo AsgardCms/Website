3469d4ac44926dd36a8341f5065aa096106a8965
Nicolas Widart <n.widart@gmail.com>
2015-04-24 21:52:20 +0200

    Add a github service

---
+++ Modules/Entry/Services/GithubService.php
@@ -0,0 +1,31 @@
+<?php namespace Modules\Entry\Services;
+
+use Github\Client;
+
+class GithubService
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+        $this->client->authenticate(config('services.github.token'), 'http_token');
+    }
+
+    /**
+     * Add a team member to AsgardCms
+     * @param string $githubUsername
+     * @return \Guzzle\Http\EntityBodyInterface|mixed|string
+     */
+    public function addTeamMember($githubUsername)
+    {
+        try {
+            return $this->client->organization()->teams()->addMember('1132536', $githubUsername);
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}
