5d51f352aff64af26b2a0223c4f6f6ab50a94447
Nicolas Widart <n.widart@gmail.com>
2015-04-24 20:02:54 +0200

    Set release stages

--- config/bugsnag.php
+++ config/bugsnag.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'api_key' => '2eea56d7f39bf9b554943853ee108ef2'
+    'api_key' => '2eea56d7f39bf9b554943853ee108ef2',
+    'notify_release_stages' => ['production', 'local']
 ];
