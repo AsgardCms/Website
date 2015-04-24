f4d4e0170444eccb9bbc9d1f19f873198ceb433a
Nicolas Widart <n.widart@gmail.com>
2015-04-24 21:51:02 +0200

    Add a github username request

---
+++ Modules/Entry/Http/Requests/GithubUsernameRequest.php
@@ -0,0 +1,19 @@
+<?php namespace Modules\Entry\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GithubUsernameRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'username' => 'required',
+            'email' => 'required|email',
+        ];
+    }
+
+    public function authorize()
+    {
+        return true;
+    }
+}
