266af07b793ceba3ad4fe8313ca675c4a8f6bb03
Nicolas Widart <n.widart@gmail.com>
2015-04-24 21:43:30 +0200

    Add and load front end routes

---
+++ Modules/Entry/Http/frontendRoutes.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+/** @var Router $router */
+
+$router->get('last/action/{token}', ['as' => 'last.action', 'uses' => 'Frontend\EntryController@validateInvite']);
+$router->post('last/action/{token}', ['as' => 'last.action.post', 'uses' => 'Frontend\EntryController@addToOrganisation']);
