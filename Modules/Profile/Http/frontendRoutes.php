b54a265f186ab88e8a63b778e3b60fc9db31a799
Nicolas Widart <n.widart@gmail.com>
2015-04-27 20:21:25 +0200

    Add a logged in middleware on account pages

--- Modules/Profile/Http/frontendRoutes.php
+++ Modules/Profile/Http/frontendRoutes.php
@@ -4,17 +4,19 @@ use Illuminate\Routing\Router;
 
 /** @var Router $router */
 
-$router->get('account', [
-    'as' => 'user.account',
-    'uses' => 'Frontend\ProfileController@show',
-]);
+$router->group(['middleware' => 'logged.in'], function (Router $router) {
+    $router->get('account', [
+        'as' => 'user.account',
+        'uses' => 'Frontend\ProfileController@show',
+    ]);
 
-$router->post('account', [
-    'as' => 'user.account.update',
-    'uses' => 'Frontend\ProfileController@update',
-]);
+    $router->post('account', [
+        'as' => 'user.account.update',
+        'uses' => 'Frontend\ProfileController@update',
+    ]);
 
-$router->get('beta-access', [
-    'as' => 'user.beta',
-    'uses' => 'Frontend\ProfileController@beta',
-]);
+    $router->get('beta-access', [
+        'as' => 'user.beta',
+        'uses' => 'Frontend\ProfileController@beta',
+    ]);
+});
