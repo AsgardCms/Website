a0beab3e45eff58c6d183e303ddd49f8138ff350
Nicolas Widart <n.widart@gmail.com>
2015-04-27 17:56:57 +0200

    Add entry relation

--- config/relations.php
+++ config/relations.php
@@ -4,6 +4,9 @@ return [
     'User' => [
         'profile' => function ($self) {
             return $self->belongsTo('Modules\Profile\Entities\Profile', 'id', 'user_id')->first();
+        },
+        'entry' => function ($self) {
+            return $self->belongsTo('Modules\Entry\Entities\Entry', 'email', 'email')->first();
         }
     ]
 ];
