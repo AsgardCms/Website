24b5e7ebdef40d67a1074fe4c0701600caf9e237
Nicolas Widart <n.widart@gmail.com>
2015-04-27 16:28:19 +0200

    Adding active classes on navigation

--- Themes/Alpha/views/partials/navigation.blade.php
+++ Themes/Alpha/views/partials/navigation.blade.php
@@ -1,11 +1,11 @@
 <h1><a href="{{ URL::to('/') }}">Asgard</a> CMS</h1>
 <nav id="nav">
     <ul>
-        <li><a href="{{ URL::to('/') }}" class="{{ Request::is('/') ? 'active' : ''}}">Home</a></li>
+        <li class="{{ on_route('homepage') ? 'active' : ''}}"><a href="{{ URL::to('/') }}">Home</a></li>
         {{--<li><a href="{{ URL::route('install') }}" class="button {{ Request::is('install') ? 'active' : ''}}">Install</a></li>--}}
-        <li><a href="{{ URL::route('faq') }}" class="{{ Request::is('faq') ? 'active' : ''}}">F.A.Q.</a></li>
-        <li><a href="{{ URL::route('doc.index') }}" class="{{ Request::is('docs*') ? 'active' : ''}}">Documentation</a></li>
-        <li>
+        <li class="{{ on_route('faq') ? 'active' : ''}}"><a href="{{ URL::route('faq') }}">F.A.Q.</a></li>
+        <li class="{{ on_route('doc.show') ? 'active' : ''}}"><a href="{{ URL::route('doc.index') }}">Documentation</a></li>
+        <li class="{{ Request::is('*auth/*') ? 'active' : ''}}">
             <a href="" class="icon fa-angle-down">Account</a>
             <ul>
                 <?php if ($user): ?>
