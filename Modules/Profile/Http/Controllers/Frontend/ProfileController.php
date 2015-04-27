b8ba57913bf54864c56b813fd0aca8b541205974
Nicolas Widart <n.widart@gmail.com>
2015-04-27 19:42:15 +0200

    Update user profile

--- Modules/Profile/Http/Controllers/Frontend/ProfileController.php
+++ Modules/Profile/Http/Controllers/Frontend/ProfileController.php
@@ -3,6 +3,8 @@
 use Modules\Core\Contracts\Authentication;
 use Modules\Core\Http\Controllers\BasePublicController;
 use Modules\Entry\Repositories\EntryRepository;
+use Modules\Profile\Http\Requests\UpdateProfileRequest;
+use Modules\User\Repositories\UserRepository;
 
 class ProfileController extends BasePublicController
 {
@@ -14,15 +16,26 @@ class ProfileController extends BasePublicController
      * @var EntryRepository
      */
     private $entry;
+    /**
+     * @var UserRepository
+     */
+    private $user;
 
     /**
      * @param Authentication $auth
+     * @param EntryRepository $entry
+     * @param UserRepository $user
      */
-    public function __construct(Authentication $auth, EntryRepository $entry)
+    public function __construct(
+        Authentication $auth,
+        EntryRepository $entry,
+        UserRepository $user
+    )
     {
         parent::__construct();
         $this->auth = $auth;
         $this->entry = $entry;
+        $this->user = $user;
     }
 
     /**
@@ -33,6 +46,13 @@ class ProfileController extends BasePublicController
         $user = $this->auth->check();
 
         return view('profile::public.profile', compact('user'));
+    }
+
+    public function update(UpdateProfileRequest $request)
+    {
+        $this->user->update($this->auth->check(), $request->all());
+
+        return redirect()->route('user.account')->with('success', 'Profile updated');
     }
 
     public function beta()
