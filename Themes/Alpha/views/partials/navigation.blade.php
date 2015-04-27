668fd20f2733d785c8a74b705c57939f64fba5b3
Nicolas Widart <n.widart@gmail.com>
2015-04-27 14:13:49 +0200

    Add account links

--- Themes/Alpha/views/partials/navigation.blade.php
+++ Themes/Alpha/views/partials/navigation.blade.php
@@ -5,5 +5,19 @@
         {{--<li><a href="{{ URL::route('install') }}" class="button {{ Request::is('install') ? 'active' : ''}}">Install</a></li>--}}
         <li><a href="{{ URL::route('faq') }}" class="{{ Request::is('faq') ? 'active' : ''}}">F.A.Q.</a></li>
         <li><a href="{{ URL::route('doc.index') }}" class="{{ Request::is('docs*') ? 'active' : ''}}">Documentation</a></li>
+        <li>
+            <a href="" class="icon fa-angle-down">Account</a>
+            <ul>
+                <?php if ($user): ?>
+                    <li><a href="{{ route('logout') }}">Logout</a></li>
+                    <?php if ($user->hasRoleName('Admin')): ?>
+                        <li><a href="{{ route('dashboard.index') }}">Admin</a></li>
+                    <?php endif; ?>
+                <?php else: ?>
+                    <li><a href="{{ route('login') }}">Login</a></li>
+                    <li><a href="{{ route('register') }}">Register</a></li>
+                <?php endif; ?>
+            </ul>
+        </li>
     </ul>
 </nav>
