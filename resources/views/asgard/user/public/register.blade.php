0391e1c162254f4519ef44bc8c5663b1f592a94a
Nicolas Widart <n.widart@gmail.com>
2015-04-27 16:34:07 +0200

    remove form labels

--- resources/views/asgard/user/public/register.blade.php
+++ resources/views/asgard/user/public/register.blade.php
@@ -14,7 +14,6 @@
             <div class="row uniform 50%">
                 <div class="12u">
                     <div class="form-group{{ $errors->has('email') ? ' has-error has-feedback' : '' }}">
-                        {!! Form::label('email', trans('user::auth.email')) !!}
                         {!! Form::text('email', Input::old('email'), ['class' => 'form-control', 'placeholder' => trans('user::auth.email')]) !!}
                         {!! $errors->first('email', '<span class="help-block">:message</span>') !!}
                     </div>
@@ -23,7 +22,6 @@
             <div class="row uniform 50%">
                 <div class="12u">
                     <div class="form-group{{ $errors->has('password') ? ' has-error has-feedback' : '' }}">
-                        {!! Form::label('password', trans('user::auth.password')) !!}
                         {!! Form::password('password', ['class' => 'form-control', 'placeholder' => trans('user::auth.password')]) !!}
                         {!! $errors->first('password', '<span class="help-block">:message</span>') !!}
                     </div>
@@ -32,7 +30,6 @@
             <div class="row uniform 50%">
                 <div class="12u">
                     <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error has-feedback' : '' }}">
-                        {!! Form::label('password_confirmation', trans('user::auth.password confirmation')) !!}
                         {!! Form::password('password_confirmation', ['class' => 'form-control', 'placeholder' => trans('user::auth.password confirmation')]) !!}
                         {!! $errors->first('password_confirmation', '<span class="help-block">:message</span>') !!}
                     </div>
