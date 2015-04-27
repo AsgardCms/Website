c3d30f65de3ea25b52b48ed87f644a149f21d854
Nicolas Widart <n.widart@gmail.com>
2015-04-27 17:19:50 +0200

    Remove sidebar link

--- Modules/Profile/Composers/SidebarViewComposer.php
+++ Modules/Profile/Composers/SidebarViewComposer.php
@@ -9,25 +9,5 @@ class SidebarViewComposer extends BaseSidebarViewComposer
 {
     public function compose(View $view)
     {
-        $view->sidebar->group(trans('core::sidebar.content'), function (SidebarGroup $group) {
-            $group->addItem('Profile', function (SidebarItem $item) {
-                $item->icon = 'fa fa-copy';
-                $item->weight = 10;
-                $item->authorize(
-                     /* append */
-                );
-                $item->addItem(trans('profile::profiles.title.profiles'), function (SidebarItem $item) {
-                    $item->icon = 'fa fa-copy';
-                    $item->weight = 0;
-                    $item->append('admin.profile.profile.create');
-                    $item->route('admin.profile.profile.index');
-                    $item->authorize(
-                        $this->auth->hasAccess('profile.profiles.index')
-                    );
-                });
-// append
-
-            });
-        });
     }
 }
