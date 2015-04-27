36b354064e009570c1d7c9220f578fa50a1cff55
Nicolas Widart <n.widart@gmail.com>
2015-04-27 18:10:42 +0200

    Make titles strong & fix link to uservoice

--- Modules/Profile/Resources/views/public/beta-access.blade.php
+++ Modules/Profile/Resources/views/public/beta-access.blade.php
@@ -11,17 +11,17 @@
             </div>
             <div class="9u 9u(2) 12u$(4) content">
                 <?php if ($entry): ?>
-                    <h4>Status</h4>
+                    <h4><strong>Status</strong></h4>
                     <?php if ($entry->accepted && $entry->activation->completed): ?>
                         <p>
                             You have access to the <strong>AsgardCms</strong> beta, congratulations! <br/>
                             You can install AsgardCms by checking out the <a href="{{ route('doc.index') }}">documentation</a>
                         </p>
-                        <h4>Feedback</h4>
+                        <h4><strong>Feedback</strong></h4>
                         <p>You can provide feedback on the following channels:</p>
                         <ul>
                             <li><a href="http://slack.asgardcms.com" target="_blank">Slack</a> for general chat and basic help</li>
-                            <li><a href="asgardcms.uservoice.com" target="_blank">Uservoice</a> for feature requests</li>
+                            <li><a href="http://asgardcms.uservoice.com" target="_blank">Uservoice</a> for feature requests</li>
                         </ul>
                     <?php elseif ($entry->accepted): ?>
                         <p>
