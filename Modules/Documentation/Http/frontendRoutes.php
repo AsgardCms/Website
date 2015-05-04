c98decd177f44f1b6d8ce7f0923efe6f08e833e6
Nicolas Widart <n.widart@gmail.com>
2015-05-04 11:09:13 +0200

    Fix route name

--- Modules/Documentation/Http/frontendRoutes.php
+++ Modules/Documentation/Http/frontendRoutes.php
@@ -6,6 +6,6 @@ use Illuminate\Routing\Router;
 $router->get('docs', ['as' => 'doc.index', 'uses' => 'Frontend\DocumentationController@index']);
 $router->get('docs/{page}', ['as' => 'doc.show', 'uses' => 'Frontend\DocumentationController@show'])->where('page', '.*');
 
-$router->post('docs/update', [
+$router->post('update/docs', [
     'uses' => 'Frontend\DocumentationController@update',
 ]);
