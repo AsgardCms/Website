44fd68080947cc9ec69c9b77bb6f5834336a0cb6
Nicolas Widart <n.widart@gmail.com>
2015-05-22 10:25:04 +0200

    Use new deploy script. Setting repo branch.

--- deploy.php
+++ deploy.php
@@ -1,20 +1,12 @@
 <?php
 
-require 'recipe/symfony.php';
+require 'recipe/laravel.php';
 
 server('staging', 'asgardcms.com', 22)
     ->user('forge')
     ->identityFile()
     ->stage('staging')
+    ->env('branch', 'develop')
     ->env('deploy_path', '/home/forge/staging.asgardcms.com');
 
 set('repository', 'git@github.com:AsgardCms/Website.git');
-
-task('deploy', [
-    'deploy:prepare',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:vendors',
-    'deploy:symlink',
-    'cleanup',
-])->desc('Deploy your project');
