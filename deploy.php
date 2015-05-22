6c308d2a97a9d4b9c5567d0d492100cc59877b31
Nicolas Widart <n.widart@gmail.com>
2015-05-22 17:15:47 +0200

    Update the deploy script

--- deploy.php
+++ deploy.php
@@ -1,6 +1,9 @@
 <?php
 
-require 'recipe/laravel.php';
+// require 'recipe/laravel.php';
+require_once 'recipe/common.php';
+
+set('keep_releases', 5);
 
 server('staging', 'asgardcms.com', 22)
     ->user('forge')
@@ -10,3 +13,28 @@ server('staging', 'asgardcms.com', 22)
     ->env('deploy_path', '/home/forge/staging.asgardcms.com');
 
 set('repository', 'git@github.com:AsgardCms/Website.git');
+
+/**
+ * Setup the environment file in the new release
+ */
+task('environment', function () {
+    run('cp /home/forge/staging.asgardcms.com/shared/.env {{release_path}}/.env');
+})->desc('Doing my stuff');
+
+// Laravel writable dirs
+set('writable_dirs', ['storage', 'vendor']);
+
+/**
+ * Main task
+ */
+task('deploy', [
+    'deploy:prepare',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:vendors',
+    'deploy:symlink',
+    'cleanup',
+    'environment',
+])->desc('Deploy your project');
+
+after('deploy', 'success');
