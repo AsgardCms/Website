19cef0488dc02a9daa4f8c072da9551f4c1db48b
Nicolas Widart <n.widart@gmail.com>
2015-06-19 14:24:48 +0200

    Adding paypal donate button

--- Themes/Alpha/views/partials/footer.blade.php
+++ Themes/Alpha/views/partials/footer.blade.php
@@ -16,6 +16,16 @@
             <ul class="copyright">
                 <li>Created by <a href="http://nicolaswidart.com">Nicolas Widart</a></li>
             </ul>
+            <ul class="copyright">
+                <li>
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                        <input type="hidden" name="cmd" value="_s-xclick">
+                        <input type="hidden" name="hosted_button_id" value="JPHG7AF5NURKQ">
+                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                    </form>
+                </li>
+            </ul>
         </footer>
 
         {!! Theme::script('js/dist/all.min.js') !!}
