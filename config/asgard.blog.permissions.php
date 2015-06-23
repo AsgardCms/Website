5f547ec328a96ca3f3875722137f2b4fe34de230
Nicolas Widart <n.widart@gmail.com>
2015-06-23 17:16:04 +0200

    Publishing package data

---
+++ config/asgard.blog.permissions.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'blog.posts' => [
+        'index',
+        'create',
+        'store',
+        'edit',
+        'update',
+        'destroy',
+    ],
+    'blog.categories' => [
+        'index',
+        'create',
+        'store',
+        'edit',
+        'update',
+        'destroy',
+    ],
+    'blog.tags' => [
+        'index',
+        'create',
+        'store',
+        'edit',
+        'update',
+        'destroy',
+    ],
+];
---
+++ config/asgard.blog.settings.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'posts-per-page' => [
+        'description'  => 'blog::settings.posts-per-page',
+        'view'         => 'text',
+        'translatable' => true,
+    ],
+    'latest-posts-amount'  => [
+        'description'  => 'blog::settings.latest-posts-amount',
+        'view'         => 'text',
+        'translatable' => true,
+    ],
+];
