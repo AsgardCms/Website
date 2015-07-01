5456ed2922d73918c2c6862dbc03b9f64bf21bf8
Nicolas Widart <n.widart@gmail.com>
2015-07-01 20:38:48 +0200

    Use url helpers

--- Themes/Alpha/views/partials/navigation.blade.php
+++ Themes/Alpha/views/partials/navigation.blade.php
@@ -1,12 +1,12 @@
 <h1><a href="{{ URL::to('/') }}">Asgard</a> CMS</h1>
 <nav id="nav">
     <ul>
-        <li class="{{ on_route('homepage') ? 'active' : ''}}"><a href="{{ URL::to('/') }}">Home</a></li>
+        <li class="{{ on_route('homepage') ? 'active' : ''}}"><a href="{{ route('homepage') }}">Home</a></li>
         <li class="{{ Request::is('*features') ? 'active' : ''}}"><a href="{{ route('page', ['features']) }}">Features</a></li>
         <li class="{{ Request::is('*install') ? 'active' : ''}}"><a href="{{ route('page', ['install']) }}" class="button">Install</a></li>
-        <li class="{{ on_route('doc.show') ? 'active' : ''}}"><a href="{{ URL::route('doc.index') }}">Documentation</a></li>
-        <li class="{{ Request::is('*blog/*') ? 'active' : ''}}"><a href="{{ URL::route(locale() . '.blog') }}">Blog</a></li>
-        <li class="{{ on_route('faq') ? 'active' : ''}}"><a href="{{ URL::route('faq') }}">F.A.Q.</a></li>
+        <li class="{{ on_route('doc.show') ? 'active' : ''}}"><a href="{{ route('doc.index') }}">Documentation</a></li>
+        <li class="{{ Request::is('*blog/*') ? 'active' : ''}}"><a href="{{ route(locale() . '.blog') }}">Blog</a></li>
+        <li class="{{ on_route('faq') ? 'active' : ''}}"><a href="{{ route('faq') }}">F.A.Q.</a></li>
         <?php if ($user): ?>
         <li class="{{ Request::is('*auth/*') ? 'active' : ''}}">
             <a href="" class="icon fa-angle-down">Account</a>
