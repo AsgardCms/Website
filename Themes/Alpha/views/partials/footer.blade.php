2d64cd24575b6ad778f0738321302b42b4858b68
Nicolas Widart <n.widart@gmail.com>
2015-07-03 12:00:04 +0200

    Add tagmanager code

--- Themes/Alpha/views/partials/footer.blade.php
+++ Themes/Alpha/views/partials/footer.blade.php
@@ -41,6 +41,15 @@
               ga('send', 'pageview');
 
             </script>
+            <!-- Google Tag Manager -->
+            <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-W4ZN7V"
+                              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-W4ZN7V');</script>
+            <!-- End Google Tag Manager -->
         <?php endif; ?>
         <script>
             // Include the UserVoice JavaScript SDK (only needed once on a page)
