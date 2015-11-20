e7d51c30f0e832c93829a2269c3add0c27e45d39
Nicolas Widart <n.widart@gmail.com>
2015-11-20 15:44:01 +0100

    Remove the named route for the direct access routes

--- Themes/Alpha/views/doc/partials/sidebar.blade.php
+++ Themes/Alpha/views/doc/partials/sidebar.blade.php
@@ -4,134 +4,134 @@
         Getting Started
     </li>
     <li class="{{ Request::is('*/getting-started/installation') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['getting-started/installation']) }}">Installation</a>
+        <a href="{{ url('docs/getting-started/installation') }}">Installation</a>
     </li>
     <li class="{{ Request::is('*/getting-started/contributing') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['getting-started/contributing']) }}">Contributing</a>
+        <a href="{{ url('docs/getting-started/contributing') }}">Contributing</a>
     </li>
     <li class="{{ Request::is('*/getting-started/used-technology') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['getting-started/used-technology']) }}">Used technology</a>
+        <a href="{{ url('docs/getting-started/used-technology') }}">Used technology</a>
     </li>
     <li class="header">
         <i class="fa fa-bolt"></i>
         Core Module
     </li>
     <li class="{{ Request::is('*/core-module/configuration') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['core-module/configuration']) }}">Configuration</a>
+        <a href="{{ url('docs/core-module/configuration') }}">Configuration</a>
     </li>
     <li class="{{ Request::is('*/core-module/navigation') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['core-module/navigation']) }}">Navigation</a>
+        <a href="{{ url('docs/core-module/navigation') }}">Navigation</a>
     </li>
     <li class="{{ Request::is('*/core-module/permissions') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['core-module/permissions']) }}">Permissions</a>
+        <a href="{{ url('docs/core-module/permissions') }}">Permissions</a>
     </li>
     <li class="{{ Request::is('*/core-module/repositories') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['core-module/repositories']) }}">Repositories</a>
+        <a href="{{ url('docs/core-module/repositories') }}">Repositories</a>
     </li>
     <li class="{{ Request::is('*/core-module/assetmanager') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['core-module/assetmanager']) }}">Asset Manager</a>
+        <a href="{{ url('docs/core-module/assetmanager') }}">Asset Manager</a>
     </li>
     <li class="{{ Request::is('*/core-module/helpers') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['core-module/helpers']) }}">Helpers</a>
+        <a href="{{ url('docs/core-module/helpers') }}">Helpers</a>
     </li>
     <li class="{{ Request::is('*/core-module/form-macros') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['core-module/form-macros']) }}">Form macros</a>
+        <a href="{{ url('docs/core-module/form-macros') }}">Form macros</a>
     </li>
     <li class="header">
         <i class="fa fa-tachometer"></i>
         Dashboard Module
     </li>
     <li class="{{ Request::is('*/dashboard-module/widgets') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['dashboard-module/widgets']) }}">Widgets</a>
+        <a href="{{ url('docs/dashboard-module/widgets') }}">Widgets</a>
     </li>
     <li class="header">
         <i class="fa fa-file-image-o"></i>
         Media Module
     </li>
     <li class="{{ Request::is('*/media-module/configuration') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['media-module/configuration']) }}">Configuration</a>
+        <a href="{{ url('docs/media-module/configuration') }}">Configuration</a>
     </li>
     <li class="{{ Request::is('*/media-module/thumbnails') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['media-module/thumbnails']) }}">Thumbnails</a>
+        <a href="{{ url('docs/media-module/thumbnails') }}">Thumbnails</a>
     </li>
     <li class="{{ Request::is('*/media-module/getting-a-thumbnail') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['media-module/getting-a-thumbnail']) }}">Getting a thumbnail</a>
+        <a href="{{ url('docs/media-module/getting-a-thumbnail') }}">Getting a thumbnail</a>
     </li>
     <li class="{{ Request::is('*/media-module/refreshing-thumbnails') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['media-module/refreshing-thumbnails']) }}">Refreshing thumbnails</a>
+        <a href="{{ url('docs/media-module/refreshing-thumbnails') }}">Refreshing thumbnails</a>
     </li>
     <li class="{{ Request::is('*/media-module/events') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['media-module/events']) }}">Events</a>
+        <a href="{{ url('docs/media-module/events') }}">Events</a>
     </li>
     <li class="{{ Request::is('*/media-module/relations') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['media-module/relations']) }}">Relations</a>
+        <a href="{{ url('docs/media-module/relations') }}">Relations</a>
     </li>
     <li class="header">
         <i class="fa fa-bars"></i>
         Menu Module
     </li>
     <li class="{{ Request::is('*/menu-module/managing-menus') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['menu-module/managing-menus']) }}">Managing menus</a>
+        <a href="{{ url('docs/menu-module/managing-menus') }}">Managing menus</a>
     </li>
     <li class="{{ Request::is('*/menu-module/displaying-menus') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['menu-module/displaying-menus']) }}">Displaying menus</a>
+        <a href="{{ url('docs/menu-module/displaying-menus') }}">Displaying menus</a>
     </li>
     <li class="header">
         <i class="fa fa-cogs"></i>
         Setting Module
     </li>
     <li class="{{ Request::is('*/setting-module/adding-settings') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['setting-module/adding-settings']) }}">Adding settings</a>
+        <a href="{{ url('docs/setting-module/adding-settings') }}">Adding settings</a>
     </li>
     <li class="{{ Request::is('*/setting-module/available-fields') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['setting-module/available-fields']) }}">Available fields</a>
+        <a href="{{ url('docs/setting-module/available-fields') }}">Available fields</a>
     </li>
     <li class="{{ Request::is('*/setting-module/custom-fields') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['setting-module/custom-fields']) }}">Custom fields</a>
+        <a href="{{ url('docs/setting-module/custom-fields') }}">Custom fields</a>
     </li>
     <li class="{{ Request::is('*/setting-module/reading-settings') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['setting-module/reading-settings']) }}">Reading settings</a>
+        <a href="{{ url('docs/setting-module/reading-settings') }}">Reading settings</a>
     </li>
     <li class="{{ Request::is('*/setting-module/events') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['setting-module/events']) }}">Events</a>
+        <a href="{{ url('docs/setting-module/events') }}">Events</a>
     </li>
     <li class="header">
         <i class="fa fa-picture-o"></i>
         Themes
     </li>
     <li class="{{ Request::is('*/themes/usage') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['themes/usage']) }}">Creating a theme</a>
+        <a href="{{ url('docs/themes/usage') }}">Creating a theme</a>
     </li>
     <li class="header">
         <i class="fa fa-user"></i>
         User Module
     </li>
     <li class="{{ Request::is('*/user-module/configuration') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['user-module/configuration']) }}">Configuration</a>
+        <a href="{{ url('docs/user-module/configuration') }}">Configuration</a>
     </li>
     <li class="{{ Request::is('*/user-module/drivers') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['user-module/drivers']) }}">Drivers</a>
+        <a href="{{ url('docs/user-module/drivers') }}">Drivers</a>
     </li>
     <li class="{{ Request::is('*/user-module/users') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['user-module/users']) }}">Users</a>
+        <a href="{{ url('docs/user-module/users') }}">Users</a>
     </li>
     <li class="{{ Request::is('*/user-module/events') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['user-module/events']) }}">Events</a>
+        <a href="{{ url('docs/user-module/events') }}">Events</a>
     </li>
     <li class="{{ Request::is('*/user-module/middleware') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['user-module/middleware']) }}">Middleware</a>
+        <a href="{{ url('docs/user-module/middleware') }}">Middleware</a>
     </li>
     <li class="{{ Request::is('*/user-module/adding-additional-user-data') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['user-module/adding-additional-user-data']) }}">Adding additional user data</a>
+        <a href="{{ url('docs/user-module/adding-additional-user-data') }}">Adding additional user data</a>
     </li>
     <li class="header">
         <i class="fa fa-cogs"></i>
         Workshop Module
     </li>
     <li class="{{ Request::is('*/workshop-module/module-scaffold') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['workshop-module/module-scaffold']) }}">Creating a module</a>
+        <a href="{{ url('docs/workshop-module/module-scaffold') }}">Creating a module</a>
     </li>
     <li class="{{ Request::is('*/workshop-module/theme-scaffold') ? 'active' : ''}}">
-        <a href="{{ route('doc.show', ['workshop-module/theme-scaffold']) }}">Creating a theme</a>
+        <a href="{{ url('docs/workshop-module/theme-scaffold') }}">Creating a theme</a>
     </li>
 </ul>
