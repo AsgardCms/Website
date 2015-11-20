9e3c575ab595e8edab74e426564e800e43c0c903
Nicolas Widart <n.widart@gmail.com>
2015-11-20 15:44:11 +0100

    Load the generated routes file

--- Modules/Documentation/Http/frontendRoutes.php
+++ Modules/Documentation/Http/frontendRoutes.php
@@ -4,7 +4,10 @@ use Illuminate\Routing\Router;
 /** @var Router $router */
 
 $router->get('docs', ['as' => 'doc.index', 'uses' => 'Frontend\DocumentationController@index']);
-$router->get('docs/{page}', ['as' => 'doc.show', 'uses' => 'Frontend\DocumentationController@show'])->where('page', '.*');
+
+include storage_path() . "/routes/documentation_routes.php";
+
+//$router->get('docs/{page}', ['as' => 'doc.show', 'uses' => 'Frontend\DocumentationController@show'])->where('page', '.*');
 
 $router->post('update/docs', [
     'uses' => 'Frontend\DocumentationController@update',
