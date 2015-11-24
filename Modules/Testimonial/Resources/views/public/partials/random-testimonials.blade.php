e2395bf777ae11d1f3bb9c071a23ed7ca323a36e
Nicolas Widart <n.widart@gmail.com>
2015-11-24 16:54:51 +0100

    Some style teaks

--- Modules/Testimonial/Resources/views/public/partials/random-testimonials.blade.php
+++ Modules/Testimonial/Resources/views/public/partials/random-testimonials.blade.php
@@ -7,7 +7,7 @@
     </div>
     <div class="row uniform 50%">
         <?php foreach ($randomTestimonials as $testimonial): ?>
-        <div class="4u">
+        <div class="4u" style="text-align: center">
             {!! $testimonial->content !!} - <a href="{{ $testimonial->url }}" target="_blank" style="font-style:italic;">{{ $testimonial->name }}</a>
         </div>
         <?php endforeach; ?>
