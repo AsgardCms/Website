550baa1de6f9ce91ba4290d8270bc55c3f8e82dc
Nicolas Widart <n.widart@gmail.com>
2015-11-24 16:51:27 +0100

    Adding box class. Centering the h3 title

--- Modules/Testimonial/Resources/views/public/partials/random-testimonials.blade.php
+++ Modules/Testimonial/Resources/views/public/partials/random-testimonials.blade.php
@@ -1,14 +1,16 @@
-<?php if ($randomTestimonials->count() !== 0): ?>
+<div class="box">
+    <?php if ($randomTestimonials->count() !== 0): ?>
     <div class="row uniform 50%">
         <div class="12u">
-            <h3>What other are saying</h3>
+            <h3 style="text-align: center">What other are saying</h3>
         </div>
     </div>
     <div class="row uniform 50%">
         <?php foreach ($randomTestimonials as $testimonial): ?>
-            <div class="4u">
-                {!! $testimonial->content !!} - <a href="{{ $testimonial->url }}" target="_blank" style="font-style:italic;">{{ $testimonial->name }}</a>
-            </div>
+        <div class="4u">
+            {!! $testimonial->content !!} - <a href="{{ $testimonial->url }}" target="_blank" style="font-style:italic;">{{ $testimonial->name }}</a>
+        </div>
         <?php endforeach; ?>
     </div>
-<?php endif; ?>
+    <?php endif; ?>
+</div>
