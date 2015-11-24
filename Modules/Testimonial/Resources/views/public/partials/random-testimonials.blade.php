47fef0c0c9f6f06ab6cf834060de234940c3b9b8
Nicolas Widart <n.widart@gmail.com>
2015-11-24 15:51:12 +0100

    Hide when no testimonial present

--- Modules/Testimonial/Resources/views/public/partials/random-testimonials.blade.php
+++ Modules/Testimonial/Resources/views/public/partials/random-testimonials.blade.php
@@ -1,12 +1,14 @@
-<div class="row uniform 50%">
-    <div class="12u">
-        <h3>What other are saying</h3>
-    </div>
-</div>
-<div class="row uniform 50%">
-    <?php foreach ($randomTestimonials as $testimonial): ?>
-        <div class="4u">
-            "{{ $testimonial->content }}" - <a href="{{ $testimonial->url }}" target="_blank" style="font-style:italic;">{{ $testimonial->name }}</a>
+<?php if ($randomTestimonials->count() !== 0): ?>
+    <div class="row uniform 50%">
+        <div class="12u">
+            <h3>What other are saying</h3>
         </div>
-    <?php endforeach; ?>
-</div>
+    </div>
+    <div class="row uniform 50%">
+        <?php foreach ($randomTestimonials as $testimonial): ?>
+            <div class="4u">
+                "{{ $testimonial->content }}" - <a href="{{ $testimonial->url }}" target="_blank" style="font-style:italic;">{{ $testimonial->name }}</a>
+            </div>
+        <?php endforeach; ?>
+    </div>
+<?php endif; ?>
