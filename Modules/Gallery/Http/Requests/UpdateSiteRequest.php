83de7c03e5f3bcea0c03c2e5726f2926f9ac29fb
Nicolas Widart <n.widart@gmail.com>
2015-12-01 20:37:23 +0100

    creating form request

---
+++ Modules/Gallery/Http/Requests/UpdateSiteRequest.php
@@ -0,0 +1,40 @@
+<?php namespace Modules\Gallery\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateSiteRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            'website_name' => 'required',
+            'website_url' => 'required',
+            'owner_name' => 'required',
+            'owner_url' => 'required',
+        ];
+    }
+
+    public function translationRules()
+    {
+        return [
+            'description' => 'required',
+        ];
+    }
+
+    public function translationMessages()
+    {
+        return [
+            'description.required' => 'The site description is required',
+        ];
+    }
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function messages()
+    {
+        return [];
+    }
+}
