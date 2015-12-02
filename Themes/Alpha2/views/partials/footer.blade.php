c809dc2ea3fd61e210b662b733386e90f390736e
Nicolas Widart <n.widart@gmail.com>
2015-12-02 14:07:02 +0100

    Use theme scripts

--- Themes/Alpha2/views/partials/footer.blade.php
+++ Themes/Alpha2/views/partials/footer.blade.php
@@ -29,7 +29,13 @@
             </ul>
         </footer>
 
-        {!! Theme::script('js/dist/all.min.js') !!}
+        {!! Theme::script('js/jquery.min.js') !!}
+        {!! Theme::script('js/jquery.dropotron.min.js') !!}
+        {!! Theme::script('js/jquery.scrollgress.min.js') !!}
+        {!! Theme::script('js/skel.min.js') !!}
+        {!! Theme::script('js/util.js') !!}
+        <!--[if lte IE 8]>{!! Theme::script('js/ie/respond.min.js') !!}<![endif]-->
+        {!! Theme::script('js/main.js') !!}
         @yield('scripts')
         <?php if (App::environment() == 'production'): ?>
             <script>
@@ -42,15 +48,6 @@
               ga('send', 'pageview');
 
             </script>
-            <!-- Google Tag Manager -->
-            <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-W4ZN7V"
-                              height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-W4ZN7V');</script>
-            <!-- End Google Tag Manager -->
         <?php endif; ?>
         <script>
             // Include the UserVoice JavaScript SDK (only needed once on a page)
