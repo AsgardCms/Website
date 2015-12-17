2bce644e282e7432300a76ab168aa497339b9a16
Nicolas Widart <n.widart@gmail.com>
2015-12-17 21:36:26 +0100

    Preparing form to submit website

--- Modules/Gallery/Resources/views/public/index.blade.php
+++ Modules/Gallery/Resources/views/public/index.blade.php
@@ -67,36 +67,93 @@
     </style>
 @stop
 @section('content')
-    <section id="main" class="container">
-        <header style="margin-bottom: 0;">
-            <h2>Sites built with AsgardCms</h2>
-        </header>
-
-        <section id="main" class="container" style="padding-top: 25px">
-            <div class="features">
-                <?php foreach ($projects as $project): ?>
-                    <div class="box special feature">
-                        <figure>
-                            <a href="{{ $project->present()->targetUrl }}" target="_blank">
-                                <img src="{{ Imagy::getThumbnail($project->image->path, 'galleryThumbnail') }}" alt="{{ $project->website_name }}"/>
-                            </a>
-                        </figure>
-                        <div class="box-content">
-                            <h3>
-                                <a href="{{ $project->present()->targetUrl }}" target="_blank">{{ $project->website_name }}</a>
-                            </h3>
-                            <small style="font-size: .8em">Developed by <a href="{{ $project->owner_url }}" target="_blank">{{ $project->owner_name }}</a></small>
-                            {!! $project->description !!}
-                        </div>
-                    </div>
-                <?php endforeach; ?>
-            </div>
+    <div class="app">
+        <section id="main" class="container" style="padding-bottom: 0;">
+            <header style="margin-bottom: 0;">
+                <h2>Sites built with AsgardCms</h2>
+                <p>
+                    <a href="" @click.prevent="openForm">Submit your own website</a>
+                </p>
+            </header>
         </section>
+        <section id="cta" style="display: none;">
+            <p>Submit finished websites you have developed with AsgardCms.</p>
 
+            <form action="{{ route('api.sites.submit') }}" method="POST" class="websiteForm" style="width: 50em;">
+                {!! Form::token() !!}
+                <div class="row uniform 50%">
+                    <div class="6u">
+                        <input type="text" name="name" id="name" placeholder="Full name" v-model="name" />
+                    </div>
+                    <div class="6u">
+                        <input type="email" name="email" id="email" placeholder="Email Address" v-model="email"/>
+                    </div>
+                    <div class="12u">
+                        <input type="text" name="website_url" id="website_url" placeholder="Website URL" v-model="website_url"/>
+                    </div>
+                    <div class="12u">
+                        <textarea name="message" id="message" placeholder="Message" v-model="message"></textarea>
+                    </div>
+                    <div class="12u">
+                        <input type="submit" value="Submit site" class="fit" @click.prevent="submitWebsite"/>
+                    </div>
+                </div>
+            </form>
+        </section>
+    </div>
+    <section id="main" class="container">
+        <div class="features">
+            <?php foreach ($projects as $project): ?>
+                <div class="box special feature">
+                    <figure>
+                        <a href="{{ $project->present()->targetUrl }}" target="_blank">
+                            <img src="{{ Imagy::getThumbnail($project->image->path, 'galleryThumbnail') }}" alt="{{ $project->website_name }}"/>
+                        </a>
+                    </figure>
+                    <div class="box-content">
+                        <h3>
+                            <a href="{{ $project->present()->targetUrl }}" target="_blank">{{ $project->website_name }}</a>
+                        </h3>
+                        <small style="font-size: .8em">Developed by <a href="{{ $project->owner_url }}" target="_blank">{{ $project->owner_name }}</a></small>
+                        {!! $project->description !!}
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
         @include('partials.call_to_action_install')
         <div style="margin-top: 5.5em;">
             @include('testimonial::public.partials.random-testimonials')
         </div>
     </section>
+@stop
 
+@section('scripts')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/1.0.11/vue.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.1.17/vue-resource.min.js"></script>
+    <script>
+        $( document ).ready(function() {
+            Vue.http.headers.common['X-CSRF-TOKEN'] = window.document.querySelector('meta#token').getAttribute('value');
+            new Vue({
+                el: '.app',
+                data: {
+                    name: '',
+                    email: 'n.widart@gmail.com',
+                    website_url: '',
+                    message: ''
+                },
+                methods: {
+                    openForm: function () {
+                        $('#cta').slideToggle();
+                    },
+                    submitWebsite: function () {
+                        var $form = $('.websiteForm'),
+                            data = {name: this.name, email: this.email, website_url: this.website_url, message: this.message};
+
+                        this.$http.post($form.attr('action'), data, function(data) {
+                        });
+                    }
+                }
+            });
+        });
+    </script>
 @stop
