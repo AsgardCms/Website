86807190d47c980168e30e10ead561288bb9422a
Nicolas Widart <n.widart@gmail.com>
2015-12-23 13:30:44 +0100

    Adding laravel vueify

--- Modules/Gallery/gulpfile.js
+++ Modules/Gallery/gulpfile.js
@@ -2,8 +2,13 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var elixir = require('laravel-elixir');
 
-var Task = elixir.Task;
+require('laravel-elixir-vueify');
 
+elixir.config.assetsPath = 'Assets/';
+elixir.config.publicPath = 'Assets/';
+elixir.config.js.outputFolder = 'dist/js';
+
+var Task = elixir.Task;
 elixir.extend('laroute', function(message) {
     new Task('laroute', function() {
         return gulp
@@ -17,8 +22,7 @@ elixir.extend('laroute', function(message) {
 elixir(function(mix) {
     mix.laroute();
 
-    //mix.browserify('main.js');
-    //
+    mix.browserify('gallery.js');
     //mix.browserSync({
     //    proxy: 'asgardcms.app',
     //    open: false
