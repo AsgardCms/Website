6f84f2426dbb7cab0a168bd7393c94f69de5ec41
Nicolas Widart <n.widart@gmail.com>
2015-12-25 23:04:02 +0100

    Loop over user module

--- Modules/Module/Http/Controllers/Frontend/ModuleController.php
+++ Modules/Module/Http/Controllers/Frontend/ModuleController.php
@@ -1,22 +1,27 @@
 <?php namespace Modules\Module\Http\Controllers\Frontend;
 
 use Illuminate\Routing\Controller;
+use Modules\Core\Http\Controllers\BasePublicController;
 use Modules\Module\Repositories\ModuleRepository;
 
-class ModuleController extends Controller
+class ModuleController extends BasePublicController
 {
     /**
      * @var ModuleRepository
      */
     private $moduleRepository;
 
+
     public function __construct(ModuleRepository $moduleRepository)
     {
+        parent::__construct();
         $this->moduleRepository = $moduleRepository;
     }
 
     public function index()
     {
-        return view('module::public.modules.index');
+        $modules = $this->moduleRepository->allForUser($this->auth->check()->id);
+
+        return view('module::public.modules.index', compact('modules'));
     }
 }
--- Modules/Module/Resources/views/public/modules/index.blade.php
+++ Modules/Module/Resources/views/public/modules/index.blade.php
@@ -10,8 +10,15 @@
                 @include('profile::public.partials.sidebar')
             </div>
             <div class="10u content">
+                <?php if ($modules->count() === 0): ?>
+                    <p>You have no modules. <a href="">Submit one.</a></p>
+                <?php endif; ?>
                 <ul>
-                    <li>Module 1</li>
+                    <?php foreach ($modules as $module): ?>
+                        <li>
+                            <a href="">{{ $module->vendor . '/' . $module->name }}</a>
+                        </li>
+                    <?php endforeach; ?>
                 </ul>
             </div>
         </div>
