ce7b6bdcaba62d5dc0bf854e458a0b1fc8be76ea
Nicolas Widart <n.widart@gmail.com>
2015-12-25 22:54:16 +0100

    Publish public assets

--- public/modules/module/js/backend_module.js
+++ public/modules/module/js/backend_module.js
@@ -1,7 +1,6 @@
 Vue.http.headers.common['X-CSRF-TOKEN'] = window.document.querySelector('meta#token').getAttribute('value');
 
 $( document ).ready(function() {
-
     new Vue({
         el: '.app',
         data: {
