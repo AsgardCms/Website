d87ac17a50a1fc45ff83da068b0782c8a703e627
Nicolas Widart <n.widart@gmail.com>
2015-12-26 14:59:11 +0100

    Adding additional columns

--- Modules/Module/Database/Migrations/2015_12_25_215500_add_user_id_column_on_modules_table.php
+++ Modules/Module/Database/Migrations/2015_12_25_215500_add_user_id_column_on_modules_table.php
@@ -14,6 +14,10 @@ class AddUserIdColumnOnModulesTable extends Migration
     {
         Schema::table('module__modules', function (Blueprint $table) {
             $table->integer('user_id')->after('id')->unsigned();
+            $table->integer('favourites')->after('changelog');
+            $table->integer('total_downloads')->after('changelog');
+            $table->integer('monthly_downloads')->after('changelog');
+            $table->integer('daily_downloads')->after('changelog');
             $table->foreign('user_id')->references('id')->on(config('auth.table', 'users'))->onDelete('cascade');
         });
     }
@@ -25,7 +29,12 @@ class AddUserIdColumnOnModulesTable extends Migration
     public function down()
     {
         Schema::table('module__modules', function (Blueprint $table) {
+            $table->dropForeign('module__modules_user_id_foreign');
             $table->dropColumn('user_id');
+            $table->dropColumn('favourites');
+            $table->dropColumn('total_downloads');
+            $table->dropColumn('monthly_downloads');
+            $table->dropColumn('daily_downloads');
         });
     }
 
