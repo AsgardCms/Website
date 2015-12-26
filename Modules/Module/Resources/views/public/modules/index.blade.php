3dd39ead81f70aeb5b62debcf34d077ec0d68c3b
Nicolas Widart <n.widart@gmail.com>
2015-12-26 13:48:14 +0100

    Adding link to submit module

--- Modules/Module/Resources/views/public/modules/index.blade.php
+++ Modules/Module/Resources/views/public/modules/index.blade.php
@@ -10,8 +10,9 @@
                 @include('profile::public.partials.sidebar')
             </div>
             <div class="10u content">
+                <a href="{{ route('p.modules.create') }}" class="button special pull-right">Submit a module</a>
                 <?php if ($modules->count() === 0): ?>
-                    <p>You have no modules. <a href="">Submit one.</a></p>
+                    <p>You have no modules. <a href="{{ route('p.modules.create') }}">Submit your first one.</a></p>
                 <?php endif; ?>
                 <ul>
                     <?php foreach ($modules as $module): ?>
