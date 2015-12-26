bf622c99ddeac2c8086b54af66165ed8c9530121
Nicolas Widart <n.widart@gmail.com>
2015-12-26 15:46:50 +0100

    Adding routes for the module gallery

--- Modules/Module/Http/publicRoutes.php
+++ Modules/Module/Http/publicRoutes.php
@@ -4,5 +4,12 @@ use Illuminate\Routing\Router;
 /** @var Router $router */
 
 $router->group(['middleware' => 'logged.in'], function (Router $router) {
+    $router->bind('singleModule', function ($id) {
+        return app(\Modules\Module\Repositories\ModuleRepository::class)->find($id);
+    });
     get('account/modules', ['uses' => 'Frontend\ModuleController@index', 'as' => 'p.modules.index']);
+    get('account/modules/create', ['uses' => 'Frontend\ModuleController@create', 'as' => 'p.modules.create']);
+    post('account/modules', ['uses' => 'Frontend\ModuleController@store', 'as' => 'p.modules.store']);
+    get('account/modules/{singleModule}/createGallery', ['uses' => 'Frontend\ModuleController@createGallery', 'as' => 'p.modules.createGallery']);
+    post('account/modules/{singleModule}/storeGallery', ['uses' => 'Frontend\ModuleController@storeGallery', 'as' => 'p.modules.storeGallery']);
 });
