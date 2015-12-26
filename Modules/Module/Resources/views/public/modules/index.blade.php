3126740c97b36a6d39f7a0953cb295c435f8b82e
Nicolas Widart <n.widart@gmail.com>
2015-12-26 13:52:47 +0100

    Small button

--- Modules/Module/Resources/views/public/modules/index.blade.php
+++ Modules/Module/Resources/views/public/modules/index.blade.php
@@ -10,7 +10,7 @@
                 @include('profile::public.partials.sidebar')
             </div>
             <div class="10u content">
-                <a href="{{ route('p.modules.create') }}" class="button special pull-right">Submit a module</a>
+                <a href="{{ route('p.modules.create') }}" class="button special small pull-right">Submit a module</a>
                 <?php if ($modules->count() === 0): ?>
                     <p>You have no modules. <a href="{{ route('p.modules.create') }}">Submit your first one.</a></p>
                 <?php endif; ?>
