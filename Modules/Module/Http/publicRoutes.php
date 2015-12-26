fb8d5d3bd540b83167d535bdd2b0e0312ca686df
Nicolas Widart <n.widart@gmail.com>
2015-12-26 13:39:47 +0100

    wrapping the routes in the logged.in middleware

--- Modules/Module/Http/publicRoutes.php
+++ Modules/Module/Http/publicRoutes.php
@@ -1,3 +1,8 @@
 <?php
 
-get('account/modules', ['uses' => 'Frontend\ModuleController@index', 'as' => 'p.modules.index']);
+use Illuminate\Routing\Router;
+/** @var Router $router */
+
+$router->group(['middleware' => 'logged.in'], function (Router $router) {
+    get('account/modules', ['uses' => 'Frontend\ModuleController@index', 'as' => 'p.modules.index']);
+});
