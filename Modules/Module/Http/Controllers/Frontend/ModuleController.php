668b8daadb0cfe28d5760a60541ec2af024fef91
Nicolas Widart <n.widart@gmail.com>
2015-12-26 15:24:10 +0100

    Fetching all categories

--- Modules/Module/Http/Controllers/Frontend/ModuleController.php
+++ Modules/Module/Http/Controllers/Frontend/ModuleController.php
@@ -1,7 +1,10 @@
 <?php namespace Modules\Module\Http\Controllers\Frontend;
 
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Modules\Core\Http\Controllers\BasePublicController;
+use Modules\Module\Http\Requests\CreateModuleRequest;
+use Modules\Module\Repositories\CategoryRepository;
 use Modules\Module\Repositories\ModuleRepository;
 
 class ModuleController extends BasePublicController
@@ -10,12 +13,16 @@ class ModuleController extends BasePublicController
      * @var ModuleRepository
      */
     private $moduleRepository;
+    /**
+     * @var CategoryRepository
+     */
+    private $categoryRepository;
 
-
-    public function __construct(ModuleRepository $moduleRepository)
+    public function __construct(ModuleRepository $moduleRepository, CategoryRepository $categoryRepository)
     {
         parent::__construct();
         $this->moduleRepository = $moduleRepository;
+        $this->categoryRepository = $categoryRepository;
     }
 
     public function index()
@@ -23,5 +30,17 @@ class ModuleController extends BasePublicController
         $modules = $this->moduleRepository->allForUser($this->auth->check()->id);
 
         return view('module::public.modules.index', compact('modules'));
+    }
+
+    public function create()
+    {
+        $categories = $this->categoryRepository->all();
+
+        return view('module::public.modules.create', compact('categories'));
+    }
+
+    public function store(CreateModuleRequest $request)
+    {
+        dd($request->all());
     }
 }
