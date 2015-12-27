bc3e1022e919a3e83d535729069b134853fd3354
Nicolas Widart <n.widart@gmail.com>
2015-12-27 15:56:46 +0100

    Include the notifications partial

--- Modules/Module/Resources/views/public/modules/index.blade.php
+++ Modules/Module/Resources/views/public/modules/index.blade.php
@@ -10,6 +10,7 @@
                 @include('profile::public.partials.sidebar')
             </div>
             <div class="10u content">
+                @include('partials.notifications')
                 <a href="{{ route('p.modules.create') }}" class="button special small pull-right">Submit a module</a>
                 <?php if ($modules->count() === 0): ?>
                     <p>You have no modules. <a href="{{ route('p.modules.create') }}">Submit your first one.</a></p>
