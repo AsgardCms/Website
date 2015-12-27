17d323190a192ae771747552263f8646b28c01dd
Nicolas Widart <n.widart@gmail.com>
2015-12-27 16:57:38 +0100

    Adding additional columns _ renaming packagist url to uri

---
+++ Modules/Module/Database/Migrations/2015_12_27_153130_add_extra_timestamps_columns_on_module.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddExtraTimestampsColumnsOnModule extends Migration
+{
+
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('module__modules', function (Blueprint $table) {
+            $table->dropColumn('is_published');
+            $table->timestamp('published_at')->after('submitted_at');
+            $table->timestamp('rejected_at')->after('submitted_at');
+            $table->renameColumn('packagist_url', 'packagist_uri');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('module__modules', function (Blueprint $table) {
+            $table->boolean('is_published')->default(false);
+            $table->dropColumn('published_at');
+            $table->dropColumn('rejected_at');
+            $table->renameColumn('packagist_uri', 'packagist_url');
+        });
+    }
+
+}
